f23b58ffbc4dd24afefaf2db09a7799d11055210
joechen <joechen@qiyi.com>
2020-11-05 21:13:39 +0800

    update firsttime

---
+++ index.js
@@ -0,0 +1,141 @@
+#!/usr/bin/env node
+
+var colors = require('colors');
+const utils = require('./utils.js');
+let fs = require("fs");
+
+//test .........................................
+//test .........................................
+
+
+//test .........................................
+//test .........................................
+
+
+//test .........................................
+//test .........................................
+
+
+
+let param = process.argv[2]; //命令行第一个参数
+console.log("param", param)
+switch (param) {
+    case "-ftp":
+        console.log("config");
+        utils.serialFtp().then((result) => {
+            utils.writeFTPConfig(JSON.stringify(result))
+        })
+
+        break;
+    case "-help":
+        console.log("-ftp               >>> step1, config your ftp setting ".yellow);
+        console.log("-files             >>> step2, config your upload path ".yellow);
+        console.log("-clear             >>> clear your settings ".yellow);
+        console.log("-view              >>> show your settings".yellow);
+        console.log("default(no param)  >>> choose one to upload ".yellow);
+
+
+
+
+        break;
+    case '-view':
+
+
+        utils.select(["0.view from folder", "1.view directly"], (answer) => {
+            let result = answer[0];
+            result = result.replace("\"", "");
+            let order = result.split(".")[0];
+
+        
+            switch (Number(order)) {
+                case 0:
+                    var exec = require('child_process').exec;
+                    console.log("__dirname", __dirname)
+                    var cmd = 'open ' + __dirname;
+                    exec(cmd);
+                    break;
+                case 1:
+                    try {
+                        let result = utils.readConfig()
+                        console.log("当前配置json", result)
+
+                    } catch (error) {
+                        console.warn("check your config, maybe it's missing".red)
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+        })
+
+
+
+
+
+
+    case "-files":
+        console.log("file");
+        utils.serialFiles().then((result) => {
+
+            utils.writeFilesConfig(JSON.stringify(result))
+        })
+
+        break;
+
+    case "-clear":
+        console.log("clear all your settings");
+        utils.select(["0.all clear", "1.clear ftp setting", "2.clear paths"], (answer) => {
+            console.log("answer", answer[0])
+            let result = answer[0];
+            result = result.replace("\"", "");
+            let order = result.split(".")[0];
+            utils.clearConfig(order)
+        })
+
+        break;
+    case undefined:
+        /*         1.Local:可以是文件名，也可以是文件夹
+        2.remote:必须是文件夹 */
+        let config = {}
+        config = utils.readConfig();
+        console.log("config", config)
+
+        let titleArray = []
+
+        config.files.map(({
+            local,
+            remote,
+            name
+        }, i) => {
+            titleArray.push(name.yellow + "=>" + local + "=>" + remote)
+        })
+
+
+
+        utils.select(titleArray, (files) => {
+            uploadFiles(files, config.ftp)
+        })
+
+        break;
+    default:
+        break;
+}
+
+
+
+//0.配置上传路径
+//1.Local:可以是文件名，也可以是文件夹
+//2.remote:必须是文件夹
+function uploadFiles(data, ftp) {
+    console.log("uploadFiles", data)
+    let local = data[0].split("=>")[1]
+    let remote = data[0].split("=>")[2]
+
+    utils.upload(ftp, {
+        local,
+        remote
+    })
+
+
+}
---
+++ utils.js
@@ -0,0 +1,226 @@
+const ipt = require("ipt");
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const fs = require('fs');
+
+const cFILES=__dirname+'/files.json';
+const cCONFIG=__dirname+'/sftp.json';
+
+
+//清除配置
+function clearConfig(order) {
+    console.log("order",order,fs)
+    switch (Number(order)) {
+        case 0:
+            fs.unlinkSync(cCONFIG);
+            fs.unlinkSync(cFILES);
+            break;
+        case 1:
+            fs.unlinkSync(cCONFIG);
+            break;
+        case 2:
+            fs.unlinkSync(cFILES);
+            break;
+        default:
+            break;
+    }
+}
+
+
+//读取配置
+function readConfig() {
+
+    let config = {}
+    let data = fs.readFileSync(cCONFIG, "utf-8")
+    config['ftp'] = JSON.parse(data);
+
+    let dataFiles = fs.readFileSync(cFILES, "utf-8")
+
+    dataFiles = dataFiles.split('\n')
+    dataFiles = dataFiles.map((single) => {
+        if (single) {
+            return JSON.parse(single)
+        }
+    })
+
+
+    config['files'] = dataFiles
+    return config;
+}
+
+
+//ftp配置保存进文件
+function writeFTPConfig(json) {
+    fs.writeFile(cCONFIG, json, {
+        flag: 'w+'
+    }, (err) => {
+        if (err) throw err;
+        console.log('文件已被保存');
+    });
+}
+
+
+function upload({
+    host,
+    port,
+    username,
+    privateKey,
+    password
+}, {
+    remote,
+    local
+}) {
+
+    let Client = require('ssh2-sftp-client');
+    let sftp = new Client();
+
+    let connectObj = {
+        host,
+        port,
+        username,
+        privateKey,
+        password
+    }
+
+    for (const key in connectObj) {
+        if(connectObj[key]=='' || !connectObj[key]) {
+            delete connectObj[key];
+        }
+        else {
+            if(key=='privateKey') {
+                connectObj[key] = fs.readFileSync(connectObj[key])
+            }
+/*             if(key=='name') {
+                connectObj["username"] = connectObj[key]
+            } */
+        }
+    }
+
+    console.log("connectObj",connectObj)
+
+    sftp.connect(connectObj).then(data => {
+        console.log('the data info:connected');
+
+        sftp.put(local, remote).then(() => {
+            console.log("upload done!".green)
+
+        }).catch((e) => {
+            console.log("catch", e)
+        })
+
+
+    }).catch(err => {
+        console.log(err, 'catch error');
+    });
+}
+
+
+//上传路径配置保存进文件
+function writeFilesConfig(json) {
+
+    try {
+        fs.readFileSync(cFILES, "utf-8")
+        json = '\n' + json;
+    } catch (error) {
+
+    }
+
+    fs.writeFile(cFILES, json, {
+        flag: 'a+'
+    }, (err) => {
+        if (err) throw err;
+        console.log('文件已被保存');
+    });
+}
+
+
+//选择框函数:标题和回调函数对应
+function select(titleArray, callback) {
+
+    ipt(titleArray, {})
+        .then(key => {
+            callback(key);
+        })
+}
+
+
+
+
+//配置host函数，输出host,port,name,privateKey等数据
+function serialFtp() {
+    let ftpConfig = {}
+    return question("host", ftpConfig).then(
+        (answer) => {
+            return question("port", ftpConfig)
+        }
+    ).then(
+        (answer) => {
+            return question("username", ftpConfig)
+        }
+    ).then(
+        (answer) => {
+            return question("password", ftpConfig)
+        }
+    ).then(
+        (answer) => {
+            return question("privateKey", ftpConfig)
+        }
+    ).then(
+        (answer) => {
+            rl.close()
+            return Promise.resolve(ftpConfig);
+        }
+    )
+}
+
+//配置host函数，输出host,port,name,privateKey等数据
+function serialFiles() {
+    let ftpConfig = {}
+    return question("name", ftpConfig).
+    then(
+        (answer) => {
+            return question("local", ftpConfig)
+        }
+    ).
+    then(
+        (answer) => {
+            let tempArr = answer.split("/")
+            let addtional = tempArr[tempArr.length - 1]
+            return question("remote", ftpConfig, addtional)
+        }
+    ).then(
+        (answer) => {
+            rl.close()
+            return Promise.resolve(ftpConfig);
+        }
+    )
+}
+
+
+function question(title, ftpConfig, addtional) {
+    return new Promise((resolve) => {
+        rl.question(title + "==>", answer => {
+            if (addtional) {
+                answer = answer + addtional
+            }
+            ftpConfig[title] = answer;
+            resolve(answer)
+        });
+    })
+}
+
+
+let utils = {
+    select,
+    serialFtp,
+    writeFTPConfig,
+    writeFilesConfig,
+    serialFiles,
+    readConfig,
+    upload,
+    clearConfig
+}
+module.exports = utils;
